9539d96bad69b31d99b11b96f811fbf2093a4cee
sagexueqi <job.xueqi@outlook.com>
2020-09-22 11:34:59 +0800

    add lizard-data-orm spring label xsd define

---
+++ lizard-data-orm/src/main/java/io/lizardframework/data/orm/spring/register/NamespaceHandler.java
@@ -0,0 +1,16 @@
+package io.lizardframework.data.orm.spring.register;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * lizard-data-orm spring label handler
+ *
+ * @author xueqi
+ * @date 2020-09-22
+ */
+public class NamespaceHandler extends NamespaceHandlerSupport {
+	@Override
+	public void init() {
+
+	}
+}
